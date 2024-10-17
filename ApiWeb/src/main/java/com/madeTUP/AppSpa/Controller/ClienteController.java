50950953894c1609a2ec0db575f7db57b46f9ef8
Marianela Holsbach <92831767+Megari94@users.noreply.github.com>
2024-10-17 14:54:00 -0300

    Update ClienteController.java

--- ApiWeb/src/main/java/com/madeTUP/AppSpa/Controller/ClienteController.java
+++ ApiWeb/src/main/java/com/madeTUP/AppSpa/Controller/ClienteController.java
@@ -180,43 +180,23 @@ public ResponseEntity<List<ClienteDTO>> getClientesD() {
     return new ResponseEntity<>(clienteDTOs, HttpStatus.OK);
 }
 
-@CrossOrigin(origins = "*")
-@GetMapping("/cliente/perfil")
-public ResponseEntity<ClientePerfilDTO> getPerfilCliente(@RequestParam Long clienteId) {
-    Cliente cliente1 = servis.findCliente(clienteId);
-    ClientePerfilDTO perfilDTO = new ClientePerfilDTO();
-    perfilDTO.setId(clienteId);
-    perfilDTO.setNombre(cliente1.getNombre());
-    perfilDTO.setApellido(cliente1.getApellido());
-
-    // No se asigna ninguna lista de sesiones, se deja como null
-    perfilDTO.setListaSesiones(null);
-
-    return ResponseEntity.ok(perfilDTO);
-}
-
-@CrossOrigin(origins = "*")
+ @CrossOrigin(origins = "*")
 @GetMapping("/clientes/traerClientesAdmin")
 public ResponseEntity<List<ClientePerfilDTO>> getClientesAdmin() {
     List<Cliente> clientes = servis.getClientes();
     List<ClientePerfilDTO> clienteDTOs = new ArrayList<>();
-
+    
     for (Cliente cliente : clientes) {
         ClientePerfilDTO clienteDTO = new ClientePerfilDTO();
+        // Establecer los atributos del DTO desde el objeto Cliente
         clienteDTO.setId(cliente.getId());
         clienteDTO.setNombre(cliente.getNombre());
         clienteDTO.setApellido(cliente.getApellido());
-
-        // Dejar la lista de sesiones como null
-        clienteDTO.setListaSesiones(new ArrayList<>());
-
-        
+        clienteDTO.setLista_Sesiones(new ArrayList<>());
         clienteDTOs.add(clienteDTO);
     }
-
+    
     return new ResponseEntity<>(clienteDTOs, HttpStatus.OK);
 }
-
-
 
 }
