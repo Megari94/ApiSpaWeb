b9366d511184cd843b5d16520fdb4f7d7b1f4ccd
Marianela Holsbach <92831767+Megari94@users.noreply.github.com>
2024-10-19 17:46:19 -0300

    Update IPersonalRepository.java

--- ApiWeb/src/main/java/com/madeTUP/AppSpa/Repository/IPersonalRepository.java
+++ ApiWeb/src/main/java/com/madeTUP/AppSpa/Repository/IPersonalRepository.java
@@ -1,10 +1,21 @@
 package com.madeTUP.AppSpa.Repository;
 
+import com.madeTUP.AppSpa.DTO.SesionPersonalDTO;
 import com.madeTUP.AppSpa.Model.Personal;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IPersonalRepository extends JpaRepository<Personal, Long> {
+@Query("SELECT new com.tu.package.SesionPersonalDTO(s.id, c.id, CONCAT(c.nombre, ' ', c.apellido), ser.nombreServicio, s.fecha, s.costo, s.asistencia) " +
+           "FROM Sesion s " +
+           "JOIN s.cliente c " +
+           "JOIN s.servicio ser " +
+           "JOIN ser.personal p " +
+           "WHERE p.id = :personalId")
+    List<SesionPersonalDTO> findSesionesByPersonalId(@Param("personalId") Long personalId);
 
 }
