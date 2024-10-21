3b6e61b69eb1f40081e05d1211477dc9510f2fb8
Marianela Holsbach <92831767+Megari94@users.noreply.github.com>
2024-10-21 19:50:28 -0300

    Update sesiones.js

--- Front/js/sesiones.js
+++ Front/js/sesiones.js
@@ -23,27 +23,24 @@ function displaySessions(sesiones) {
     const tableBody = document.querySelector('tbody');
     tableBody.innerHTML = ''; // Limpiar la tabla
 
-    // Filtrar las sesiones que tienen asistencia igual a "SOLICITADO"
-    const sesionesFiltradas = sesiones.filter(sesion => sesion.asistencia === 'SOLICITADO');
-
-    sesionesFiltradas.forEach((sesion) => {
+    sesiones.forEach((sesion) => {
         const row = document.createElement('tr');
         const fechaFormateada = formatearFecha(sesion.fecha);
 
-        row.innerHTML = `
+        row.innerHTML = 
             <td>${sesion.id}</td>
             <td>${sesion.asistencia}</td>
             <td>${sesion.costo}</td>
             <td>${fechaFormateada}</td>
-            <td>${sesion.nombre_completo}</td>
+            <td>${sesion.nombre_completo} </td>
             <td>${sesion.nombre_servicio}</td>
             <td>
                 <div class="button-group">
-                    <button class="btn-aceptar" onclick="aceptarSolicitud('${sesion.id}', '${sesion.nombre_completo}', '${sesion.nombre_servicio}', ${sesion.costo})">Aceptar</button>
+                    <button class="btn-aceptar" onclick="aceptarSolicitud('${sesion.id}', '${sesion.nombre_completo}, '${sesion.nombre_servicio}', ${sesion.costo})">Aceptar</button>
                     <button class="btn-rechazar" onclick="rechazarSolicitud('${sesion.id}')">Denegar</button>
                 </div>
             </td>
-        `;
+        ;
 
         tableBody.appendChild(row);
     });
@@ -51,7 +48,6 @@ function displaySessions(sesiones) {
 
 // Llamar a la función al cargar la página
 window.onload = loadSessionsAdmin;
-
 
 
 function formatearFecha(fechaISO) {
