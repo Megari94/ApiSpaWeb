417521377007675769667e90d69d55c63dcdef27
Marianela Holsbach <92831767+Megari94@users.noreply.github.com>
2024-10-21 15:45:18 -0300

    Update ServicioController.java

--- ApiWeb/src/main/java/com/madeTUP/AppSpa/Controller/ServicioController.java
+++ ApiWeb/src/main/java/com/madeTUP/AppSpa/Controller/ServicioController.java
@@ -12,6 +12,8 @@ import com.madeTUP.AppSpa.Model.Servicio;
 import com.madeTUP.AppSpa.Service.IPersonalService;
 import com.madeTUP.AppSpa.Service.IServicioService;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
