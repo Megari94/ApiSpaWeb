d5571ffddcd3a9cb812e881fc525707371627643
Marianela Holsbach <92831767+Megari94@users.noreply.github.com>
2024-10-21 10:09:43 -0300

    Update AdministradorController.java

--- ApiWeb/src/main/java/com/madeTUP/AppSpa/Controller/AdministradorController.java
+++ ApiWeb/src/main/java/com/madeTUP/AppSpa/Controller/AdministradorController.java
@@ -38,6 +38,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "https://apispaweb-production.up.railway.app")
 public class AdministradorController {
     @Autowired 
     private IAdministradorService servis;
