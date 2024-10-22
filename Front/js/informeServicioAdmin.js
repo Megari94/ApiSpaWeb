899893129954a79fa63d7c1feec9c30d05394190
Marianela Holsbach <92831767+Megari94@users.noreply.github.com>
2024-10-22 04:47:16 -0300

    Update informeServicioAdmin.js

--- Front/js/informeServicioAdmin.js
+++ Front/js/informeServicioAdmin.js
@@ -65,11 +65,11 @@ async function generarInforme() {
         doc.setFontSize(10);
         doc.setFont("Helvetica", "bold");
         doc.text("ID", 50, 250);
-        doc.text("Asistencia", 100, 250); // Reducción de espacio
+        doc.text("Asistencia", 80, 250); // Reducción de espacio
         doc.text("Costo", 180, 250); // Reducción de espacio
-        doc.text("Fecha", 280, 250); // Reducción de espacio
-        doc.text("Nombre Completo", 405, 250); // Reducción de espacio
-        doc.text("Nombre Servicio", 500, 250); // Reducción de espacio
+        doc.text("Fecha", 250, 250); // Reducción de espacio
+        doc.text("Nombre Completo", 385, 250); // Reducción de espacio
+        doc.text("Nombre Servicio", 480, 250); // Reducción de espacio
 
         // Dibujar una línea para el encabezado
         doc.line(30, 255, 580, 255);
@@ -86,11 +86,11 @@ async function generarInforme() {
             doc.setFontSize(10);
             doc.setFont("Helvetica", "normal");
             doc.text(servicio.id.toString(), 50, yPosition); // ID
-            doc.text(servicio.asistencia, 100, yPosition); // Asistencia
+            doc.text(servicio.asistencia, 80, yPosition); // Asistencia
             doc.text(servicio.costo.toFixed(2).toString(), 180, yPosition); // Costo
-            doc.text(servicio.fecha ? new Date(servicio.fecha).toLocaleString() : '', 280, yPosition); // Fecha
-            doc.text(servicio.nombre_completo, 405, yPosition); // Nombre completo
-            doc.text(servicio.nombre_servicio, 500, yPosition); // Nombre servicio
+            doc.text(servicio.fecha ? new Date(servicio.fecha).toLocaleString() : '', 250, yPosition); // Fecha
+            doc.text(servicio.nombre_completo, 385, yPosition); // Nombre completo
+            doc.text(servicio.nombre_servicio, 480, yPosition); // Nombre servicio
 
             totalCosto += servicio.costo; // Sumar al total
             yPosition += 15; // Espaciado entre filas reducido
@@ -99,7 +99,7 @@ async function generarInforme() {
         // Añadir la suma total de costos al final
         doc.setFontSize(12);
         doc.setFont("Helvetica", "bold");
-        doc.text(`Total Costo: $${totalCosto.toFixed(2)}`, 410, yPosition + 20); // Posición para mostrar el total
+        doc.text(`Total Costo: $${totalCosto.toFixed(2)}`, 410, yPosition + 40); // Posición para mostrar el total
 
         // Generar Blob
         pdfBlob = doc.output('blob');
