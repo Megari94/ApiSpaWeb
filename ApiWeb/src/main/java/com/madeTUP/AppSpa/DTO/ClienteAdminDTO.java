dabd463f8e814a96b2e80eaa8deff6e8e2200539
Marianela Holsbach <92831767+Megari94@users.noreply.github.com>
2024-10-22 14:38:27 -0300

    Update ClienteAdminDTO.java

--- ApiWeb/src/main/java/com/madeTUP/AppSpa/DTO/ClienteAdminDTO.java
+++ ApiWeb/src/main/java/com/madeTUP/AppSpa/DTO/ClienteAdminDTO.java
@@ -9,16 +9,21 @@ public class ClienteAdminDTO {
      private String nombre;
     private String apellido;
      private String correo;
+     private String contrasenia;
+    private String nombre_usuario;
 
     public ClienteAdminDTO() {
     }
 
-    public ClienteAdminDTO(Long id, String nombre, String apellido, String correo) {
+    public ClienteAdminDTO(Long id, String nombre, String apellido, String correo, String contrasenia, String nombre_usuario) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.correo = correo;
+        this.contrasenia = contrasenia;
+        this.nombre_usuario = nombre_usuario;
     }
+
      
      
 }
