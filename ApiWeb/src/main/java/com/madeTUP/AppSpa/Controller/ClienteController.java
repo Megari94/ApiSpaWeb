b9cca959dacfb8284429140a4a9118d68ac99aa1
Marianela Holsbach <92831767+Megari94@users.noreply.github.com>
2024-10-22 06:03:40 -0300

    Update ClienteController.java

--- ApiWeb/src/main/java/com/madeTUP/AppSpa/Controller/ClienteController.java
+++ ApiWeb/src/main/java/com/madeTUP/AppSpa/Controller/ClienteController.java
@@ -39,6 +39,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Virginia
  */
 @RestController
+@CrossOrigin(origins = "https://apispaweb-production.up.railway.app")
 public class ClienteController {
     @Autowired
     private IClienteService servis;
