59b61e254b8693bb10552953e4fa1c95265a500f
Marianela Holsbach <92831767+Megari94@users.noreply.github.com>
2024-10-22 03:50:50 -0300

    Update ClienteAdminDTO.java

--- ApiWeb/src/main/java/com/madeTUP/AppSpa/DTO/ClienteAdminDTO.java
+++ ApiWeb/src/main/java/com/madeTUP/AppSpa/DTO/ClienteAdminDTO.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.madeTUP.AppSpa.DTO;
 
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- *
- * @author Virginia
- */
 @Setter @Getter
 public class ClienteAdminDTO {
       private Long id;
@@ -27,36 +19,6 @@ public class ClienteAdminDTO {
         this.apellido = apellido;
         this.correo = correo;
     }
-
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    
-
-public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
+     
+     
 }
