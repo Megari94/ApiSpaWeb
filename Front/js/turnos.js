00263b25f598beeb81d8be8dcff2c8c70a54e4b5
Marianela Holsbach <92831767+Megari94@users.noreply.github.com>
2024-10-26 21:37:38 -0300

    Update turnos.js

--- Front/js/turnos.js
+++ Front/js/turnos.js
@@ -316,116 +316,105 @@ async function generarFactura(turnoId) {
     const { jsPDF } = window.jspdf;
     const doc = new jsPDF('p', 'pt', 'a4');
 
-    // Establecer la fecha actual en el campo de fecha 
-    const today = new Date().toISOString().split('T')[0]; // Obtener la fecha actual en formato YYYY-MM-DD
-    document.getElementById('invoiceDate').value = today; // Asignar la fecha actual al campo de entrada
+    const today = new Date().toISOString().split('T')[0];
+    document.getElementById('invoiceDate').value = today;
 
     const invoiceDate = document.getElementById('invoiceDate').value;
     const clientName = document.getElementById('clientName').value;
-    const totalAmount = document.getElementById('totalAmount').value;
+    const totalAmount = parseFloat(document.getElementById('totalAmount').value) || 0;
 
-    // Validar campos obligatorios
-    if (!invoiceDate || !clientName || totalAmount === '0.00') {
-        alert("Por favor complete todos los campos.");
+    if (!invoiceDate || !clientName || totalAmount === 0) {
+        alert("Por favor complete todos los campos obligatorios.");
         return;
     }
 
-    // Título y encabezado
-    doc.setFontSize(14);
-    doc.setFont("Helvetica", "bold");
-    doc.text("SENTIRSE BIEN", 70, 75);
-    doc.setFontSize(12);
-    doc.setFont("Helvetica", "normal");
-
-    // Bordes para la estructura de la factura
-    doc.rect(30, 30, 550, 750); // Borde general
-    doc.rect(30, 30, 550, 120); // Borde encabezado
-    doc.rect(30, 700, 550, 50); // Borde subtotal y total
-
-    // Dibuja un rectángulo alrededor de la "C"
-    const cX = 290;
-    const cY = 50;
-    const cWidth = 40;
-    const cHeight = 40;
-    doc.rect(cX, cY, cWidth, cHeight); // Dibuja el rectángulo
-    doc.setFontSize(30);
-    doc.text("C", cX + 13, cY + 30); // Centra la "C" dentro del rectángulo
-    doc.setFontSize(12);
-
-    // Detalles de la factura a la derecha
-    doc.setFontSize(16);
-    doc.setFont("Helvetica", "bold");
-    doc.text("Detalle de Factura", 380, 65);
-
-    // Obtener la fecha actual y formatearla
-    const fechaActual = new Date();
-    const dia = fechaActual.getDate().toString().padStart(2, '0');
-    const mes = (fechaActual.getMonth() + 1).toString().padStart(2, '0');
-    const anio = fechaActual.getFullYear();
-    const fechaFormateada = `${dia}/${mes}/${anio}`;
-    doc.text("Fecha de Emisión: " + fechaFormateada, 380, 95);
-
-    doc.text("CUIT: 27316471566", 380, 110);
-    doc.text("Ingresos Brutos: EXENTO", 380, 125);
-    doc.text("Fecha de Inicio de Actividades: ", 380, 140);
-
-    // Datos de recibo
-    doc.text("Recibí de: " + clientName, 40, 170);
-    doc.text("DNI: ", 350, 170);
-    doc.text("Domicilio: ", 40, 190);
-    doc.text("Localidad:", 40, 210);
-    doc.text("Provincia: ", 350, 210);
-    doc.text("Tipo de Ingreso: PRODUCIDOS PROPIOS ", 40, 240);
-    doc.text("Medio de Pago recibido: TRANSFERENCIA A CTA 10071/08 Nº BNA", 40, 260);
-    doc.text("Concepto ", 40, 280);
-
-    // Encabezado de la tabla
-    doc.setFontSize(10);
-    doc.setFont("Helvetica", "bold");
-    doc.text("Código", 50, 330);
-    doc.text("Producto / Servicio", 160, 330);
-    doc.text("Precio Unit.", 550, 330, { align: "right" });
-    doc.line(30, 335, 580, 335); // Línea para el encabezado
-
-    // Añadir los datos del servicio/producto
-    const serviceRows = document.querySelectorAll('.service-row');
-    let yOffset = 350;
-    let totalServicios = 0;
-    serviceRows.forEach((row) => {
-        const serviceId = row.querySelector('.service-id').value;
-        const serviceName = row.querySelector('.service-name').value;
-        const serviceCost = parseFloat(row.querySelector('.service-cost').value) || 0;
-
+    const encabezado = () => {
+        doc.setFontSize(14);
+        doc.setFont("Helvetica", "bold");
+        doc.text("SENTIRSE BIEN", 70, 75);
+        doc.setFontSize(12);
         doc.setFont("Helvetica", "normal");
-        doc.text(serviceId, 60, yOffset);
-        const productoDividido = doc.splitTextToSize(serviceName, 180);
-        doc.text(productoDividido, 160, yOffset);
-        doc.text(serviceCost.toFixed(2), 550, yOffset, { align: "right" });
 
-        yOffset += 20;
-        doc.line(30, yOffset, 580, yOffset); // Línea debajo de la fila de producto
-        yOffset += 20;
+        doc.rect(30, 30, 550, 750);
+        doc.rect(30, 30, 550, 120);
+        doc.rect(30, 700, 550, 50);
 
-        totalServicios += serviceCost;
-    });
+        doc.setFontSize(30);
+        doc.text("C", 303, 80);
+        doc.rect(290, 50, 40, 40);
+        
+        doc.setFontSize(16);
+        doc.text("Detalle de Factura", 380, 65);
+        doc.setFontSize(12);
+        const fechaActual = new Date();
+        const dia = fechaActual.getDate().toString().padStart(2, '0');
+        const mes = (fechaActual.getMonth() + 1).toString().padStart(2, '0');
+        const anio = fechaActual.getFullYear();
+        const fechaFormateada = `${dia}/${mes}/${anio}`;
+        doc.text("Fecha de Emisión: " + fechaFormateada, 380, 95);
 
-    // Subtotal y total
-    doc.setFontSize(12);
-    doc.setFont("Helvetica", "bold");
-    doc.text("Importe Total: $" + totalServicios.toFixed(2), 400, 727);
-    doc.line(30, 735, 580, 735); // Línea final estilo footer
-    doc.text("Total de servicios: $" + totalServicios.toFixed(2), 40, 750);
+        doc.text("CUIT: 27316471566", 380, 110);
+        doc.text("Ingresos Brutos: EXENTO", 380, 125);
+        doc.text("Fecha de Inicio de Actividades: ", 380, 140);
 
-    // Convertir PDF a Blob para descarga
+        doc.text("Recibí de: " + clientName, 40, 170);
+        doc.text("DNI: ", 350, 170);
+        doc.text("Domicilio: ", 40, 190);
+        doc.text("Localidad:", 40, 210);
+        doc.text("Provincia: ", 350, 210);
+        doc.text("Tipo de Ingreso: PRODUCIDOS PROPIOS ", 40, 240);
+        doc.text("Medio de Pago recibido: TRANSFERENCIA A CTA 10071/08 Nº BNA", 40, 260);
+        doc.text("Concepto ", 40, 280);
+    };
+
+    const agregarServicios = () => {
+        doc.setFontSize(10);
+        doc.setFont("Helvetica", "bold");
+        doc.text("Código", 50, 330);
+        doc.text("Producto / Servicio", 160, 330);
+        doc.text("Precio Unit.", 550, 330, { align: "right" });
+        doc.line(30, 335, 580, 335);
+
+        let yOffset = 350;
+        let totalServicios = 0;
+        document.querySelectorAll('.service-row').forEach((row) => {
+            const serviceId = row.querySelector('.service-id').value;
+            const serviceName = row.querySelector('.service-name').value;
+            const serviceCost = parseFloat(row.querySelector('.service-cost').value) || 0;
+
+            doc.setFont("Helvetica", "normal");
+            doc.text(serviceId, 60, yOffset);
+            const productoDividido = doc.splitTextToSize(serviceName, 180);
+            doc.text(productoDividido, 160, yOffset);
+            doc.text(serviceCost.toFixed(2), 550, yOffset, { align: "right" });
+
+            yOffset += 20;
+            doc.line(30, yOffset, 580, yOffset);
+            yOffset += 20;
+
+            totalServicios += serviceCost;
+        });
+        return totalServicios;
+    };
+
+    const subtotalYTotal = (totalServicios) => {
+        doc.setFontSize(12);
+        doc.setFont("Helvetica", "bold");
+        doc.text("Importe Total: $" + totalServicios.toFixed(2), 400, 727);
+        doc.line(30, 735, 580, 735);
+        doc.text("Total de servicios: $" + totalServicios.toFixed(2), 40, 750);
+    };
+
+    encabezado();
+    const totalServicios = agregarServicios();
+    subtotalYTotal(totalServicios);
+
     const pdfBlob = doc.output('blob');
-
-    // Descargar automáticamente el PDF
     const link = document.createElement('a');
     link.href = URL.createObjectURL(pdfBlob);
     link.download = "factura.pdf";
     link.click();
 
-    // Limpiar el formulario después de generar la factura
     setTimeout(() => {
         document.getElementById('invoiceDate').value = today;
         document.getElementById('clientName').value = '';
